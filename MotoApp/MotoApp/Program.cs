9589ccf99f76df9f4b35fd10fdd80f12d3cd6f6d
Bartosz <bartosz.kulinski1@gmail.com>
2024-01-30 21:31:57 +0100

    commit generic interface with sql and Entity

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,9 +1,34 @@
-﻿using MotoApp.Entities;
+﻿using MotoApp.Data;
+using MotoApp.Entities;
 using MotoApp.Repositories;
 
-var employeeRepository = new GenericRepository<Employee>();
-employeeRepository.Add(new Employee { FirstName = "Adam" });
-employeeRepository.Add(new Employee { FirstName = "Jakub" });
-employeeRepository.Add(new Employee { FirstName = "Marek" });
+var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
+AddEmployees(employeeRepository);
+AddManagers(employeeRepository);
+WriteAllToConsole(employeeRepository);
 
-employeeRepository.Save();
+
+static void AddEmployees(IRepository<Employee> employeeRepository)
+{
+    employeeRepository.Add(new Employee { FirstName = "Adam" });
+    employeeRepository.Add(new Employee { FirstName = "Jakub" });
+    employeeRepository.Add(new Employee { FirstName = "Marek" });
+    employeeRepository.Save();
+}
+
+static void AddManagers(IWriteRepository<Manager> managerRepository)
+{
+    managerRepository.Add(new Manager { FirstName = "Adam" });
+    managerRepository.Add(new Manager { FirstName = "Jakub" });
+    managerRepository.Add(new Manager { FirstName = "Marek" });
+    managerRepository.Save();
+}
+
+static void WriteAllToConsole(IReadRepository<IEntity> repository)
+{
+    var items = repository.GetAll();
+    foreach (var item in items)
+    {
+        Console.WriteLine(item);
+    }
+}
